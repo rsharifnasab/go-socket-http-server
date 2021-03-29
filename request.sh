1a15e25369d53fb3437e09a4dcf2fe5adb4c03ca
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-03-29 21:38:42 +0430

    add more examples

--- request.sh
+++ request.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-curl -vv 127.0.0.1:8080
+curl 127.0.0.1/index.html
+# or use verbose mode: view sending and receiving headers
+curl -v 127.0.0.1/index.html 
+
+http GET 127.0.0.1/humans.txt
