06059165589ed7d9d2b8def9ae0164eaa2c72999
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-03-29 21:41:33 +0430

    request sh more detail

--- request.sh
+++ request.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+printf "\nget index.html with curl\n"
 curl 127.0.0.1/index.html
 # or use verbose mode: view sending and receiving headers
-curl -v 127.0.0.1/index.html 
+printf "\nget humans.txt in verbose mode\n"
+curl -v 127.0.0.1/humans.txt
 
-http GET 127.0.0.1/humans.txt
+printf "\nget / with http client\n"
+http GET 127.0.0.1
